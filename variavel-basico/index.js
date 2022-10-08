45666b8e9ac5190cbfef4c452303ab469c295de1
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2022-10-08 16:28:28 -0300

    básico 0.1

---
+++ variavel-basico/index.js
@@ -0,0 +1,14 @@
+var nome = window.prompt('Qual é o seu nome?'); // armazena a entrada de dados na variável
+window.alert('Prazer em te conhecer,' +  nome + '!'); // apresenta um prefixo genérico + o valor armazenado na variável
+
+var x1 = Number(window.prompt('Digite um valor')); // O parseInt converte uma string para Number.
+var x2 = Number(window.prompt('Digite outro valor')); // //  //  //  //   //  //  // //  // // 
+var soma = x1 + x2 // Soma o valores de X1 e X2 ou concatenona eles, dependendo do tipo da variável
+
+/* O "Number" no código automaticamente detecta se o número digitado é inteiro ou real */ 
+// String(n) converte para string 
+// n.toString(n) mesma coisa que acima. 
+window.alert('Resultado é igual á: ' + soma);
+
+
+//Data de criação do documento: 08/10/2022
---
+++ variavel-basico/index2.js
@@ -0,0 +1,24 @@
+var nome = 'Murilo' 
+var idade = 17 
+var namorada = 'Emilly' 
+var profissão = 'Programador' 
+
+document.write(`Seu nome é ${nome} e tem ${idade} anos. O nome da sua namorada é ${namorada} e sua profissão é ${profissão}`);
+
+// utilizando sifrão e as chaves podemos mostrar na tela valores da variável de um modo mais morderno. 
+//document.write serve para escreve na tela.
+//É possível utilizar tags HTMl dentro do "Document.Write" ;-;
+
+// Data de criação do documento: 08/10/2022
+
+//FORMATAÇÕES DE STRINGS 
+
+//s.toUpperCase() Deixa a String maiúscula
+//s.toLowerCase() Deixa a String minúscula 
+//s.length mostra o tamanho da String 
+
+//FORMATAÇÃO DE NÚMEROS 
+
+// toFixed() aumenta a quantidade de casas decimais 
+// toFixed.repleace('.' , ',') para trocar o ponto pela a vírgula 
+//n.toLocaleString('pt-br' {style: 'currency' , 'currency': 'BRL'})
