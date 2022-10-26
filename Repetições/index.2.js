bdbc8b3d3a3a3c4ce5192888f1d71f75996e0686
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2022-10-26 17:33:19 -0300

    repetições 1.0

---
+++ Repetições/index.2.js
@@ -0,0 +1,5 @@
+for(var c = 1; c <= 10; c++){
+    console.log(c)
+}
+
+// laço mais completo com 3 fases, sendo a inicialização, onde declaramos a variável, o teste lógico onde declaramos uma condição e o incremento. funcionando da mesma forma que outros tipos de laços, enquanto o teste lógico for verdadeiro o loop acontecerá. 
