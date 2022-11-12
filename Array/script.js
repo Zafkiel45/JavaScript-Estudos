2b601433fcfe7590dae7de6012f095676e5b9d5c
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2022-11-12 15:56:26 -0300

    Arrays base concluído

--- Array/script.js
+++ Array/script.js
@@ -3,7 +3,19 @@ let num = [1,29,39,28,11,299,29999]
 num[7] = 800 //adiciona um elemento na posição 7
 num.push(200) //adiciona mais um elemento ao final do array
 num.sort() // organiza os elementos em ordem crescente
+let pos = num.indexOf(28) // o IndexOf busca a chave de indentificação do valor solicitado. Quando se busca uma chave que não existe, o script retorna um valor -1
 
+
+console.log(pos)
+/*
 for(var c = 0; c < num.length; c++){
     console.log(num[c])
 }
+*/
+
+//laço otimizado para arrays. Nele precisamos colocar apenas o nome da variável(Inicializador) e o nome da variável que está armazenando o array. 
+
+/* 
+for(let x in num){
+    console.log(num[x])
+} */
