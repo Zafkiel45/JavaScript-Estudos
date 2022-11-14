a55a820f1f9b09c388ab4826a3418f04036da322
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2022-11-14 15:25:03 -0300

    Matemática Complexa

---
+++ Matemática/script.js
@@ -0,0 +1,20 @@
+var teste = 10
+
+console.log(Math.pow(teste,2)) // eleva o valor armazenado ao quadrado, podendo o expoente ser qualquer número.
+console.log(Math.round(0.6)) // Arredonda o valor para o inteiro mais próximo. 
+console.log(Math.ceil(1.6)) // arredonda pra cima
+console.log(Math.floor(1.6)) // arredonda pra baixo
+console.log(Math.abs(teste))// valor absoluto
+console.log(Math.max(2,5,3)) // retorna o maior argumento/ sendo esse argumento o maior valor entre os 3. 
+console.log(Math.min(10,2,299))// retorna o menor valor 
+console.log(Math.PI) //Circuferência de um círculo
+console.log(Math.E) // A base do logaritmo natural
+console.log(Math.sqrt(3)) // Raiz quadrada
+console.log(Math.pow(3, 1/3)) // tira a raiz cúbica
+console.log(Math.sin(30)) // trignometria
+console.log(Math.log(20)) // logaritmo natural
+console.log(Math.log(100)/Math.LN10) //Logaritmo de base 10 de 100. 
+console.log(Math.log(512)/Math.LN2) // logaritmo de 2 de 512
+console.log(Math.exp(3)) //Math.E ao cubo 
+console.log(isNaN('teste')) //Retorna verdadeiro se o valor for NaN, um valor não número como uma string ou objeto
+console.log(isFinite(-21/0)) //retorna verdadeiro se o valor não for NaN, infinito ou -ifinito
