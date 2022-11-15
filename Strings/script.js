7266f187eeb6db7f66b9660cde334729f18c37fe
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2022-11-15 09:56:03 -0300

    em andamento

---
+++ Strings/script.js
@@ -0,0 +1,7 @@
+var texto = 'test\
+teste\
+teste'
+
+//Uma string deve ser escritan a mesma linha, mas para que possa colocar uma string em várias linhas, mas que seja exibida na mesma linha, basta colocar o barra como demostrado acima. 
+
+console.log(texto);
