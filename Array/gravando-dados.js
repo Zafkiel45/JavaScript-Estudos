af798ffa4ae80ddd956c14708ce0572eafa36905
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2023-03-10 14:55:06 -0300

    array testes

---
+++ Array/gravando-dados.js
@@ -0,0 +1,13 @@
+// let i = 3
+// let group = [i]
+// group[i] = 2
+// console.log(group[i])
+
+// let teste = []
+// teste[-2] = 10
+// console.log(teste[-2])
+
+// let tesstes = []
+// tesstes[1.5] = 'orange'
+
+// console.log(tesstes.indexOf('orange'))
